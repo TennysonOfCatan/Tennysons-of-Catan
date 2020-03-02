66743d083dfcfd85d0cf12dfb327b047140328ab
DeclannDreams <54551956+DeclannDreams@users.noreply.github.com>
2020-03-02 10:46:38 -0600

    Add to separate folder for these

---
+++ Resource.py
@@ -0,0 +1,22 @@
+import pygame, unit, helper, bmpfont
+from pygame.sprite import Sprite
+
+SIZE = 50
+
+class Resource(Sprite):
+
+    def __init__(self, tile-x = None, tile_y = None, Number = None **keywords):
+        Sprite.__init__(self)
+
+        #turn keywords off for now
+        self.Number = number
+
+        #pygame reqs
+        self.image = None
+        self.rect = pygame.Rect(0, 0, SIZE, SIZE)
+
+
+
+
+
+        
